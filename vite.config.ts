b9e8833e0d4f2fa905599d8faa6207d882806b9b
Roman Zubko <romanzubko93@gmail.com>
2023-11-29 12:29:30 +0200

    vite.config base link fix

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/rtk_query-voting-app'
+  base: '/rtk_query-voting-app/'
 })
